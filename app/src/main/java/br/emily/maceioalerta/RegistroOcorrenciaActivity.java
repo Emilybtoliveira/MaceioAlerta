18a76654abe52a09fb223fd9d56e8e92bf336639
Emily <emilybtoliveira@gmail.com>
2019-10-21 00:38:39 -0300

    modifica icon e ocorrencia

---
+++ app/src/main/java/br/emily/maceioalerta/RegistroOcorrenciaActivity.java
@@ -0,0 +1,14 @@
+package br.emily.maceioalerta;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroOcorrenciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_ocorrencia);
+    }
+}
